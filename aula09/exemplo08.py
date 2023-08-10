b2485bbd9a15dd749040f0a11f3287efe4078b2f
Luiz Gustavo Cruz Freire <136338594+HERMEUSMOURA@users.noreply.github.com>
2023-08-10 15:54:29 -0400

    Add files via upload

---
+++ aula09/exemplo08.py
@@ -0,0 +1,9 @@
+#ler um nome e email e armazenar no arquivo dados3.txt
+
+try:
+    txt = open("aula09/dados3.txt","a+",encoding='utf-8')
+    nome = input("informe o nome:")
+    email = input("informe o email: ")
+    txt.write(f"{nome} - {email}")
+except:
+    print("error ao gravar os dados")
