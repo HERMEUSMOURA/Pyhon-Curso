b7758d9eddfa335ae7d8092d1ab9f4c0cec61434
Luiz Gustavo Cruz Freire <136338594+HERMEUSMOURA@users.noreply.github.com>
2023-08-10 15:55:58 -0400

    Add files via upload

--- aula09/exemplo08.py
+++ aula09/exemplo08.py
@@ -4,6 +4,6 @@ try:
     txt = open("aula09/dados3.txt","a+",encoding='utf-8')
     nome = input("informe o nome:")
     email = input("informe o email: ")
-    txt.write(f"{nome} - {email}")
+    txt.write(f"{nome:^15} - {email:^15}\n")
 except:
     print("error ao gravar os dados")
